aac44681121adc122ccb314886b6d734850446a3
Akhil kg <akhilkg5869@gmail.com>
2026-07-31 00:14:49 +0530

    it is app form

---
+++ my-todo-app/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h2>TODO APP</h2>
+      <form >
+        <input type="text" placeholder='enter your message' />
+        <button>ADD</button>
+      </form>
+      
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
