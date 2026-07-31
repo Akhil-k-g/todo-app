a3008da73ffb764a53d0d26103ed95a0919ab4c2
Akhil kg <akhilkg5869@gmail.com>
2026-07-31 11:33:06 +0530

    todo adding and listing

--- my-todo-app/src/App.jsx
+++ my-todo-app/src/App.jsx
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 function App() {
+  
+  const inputRef = useRef()
+
+  const [todo,setTodo] =useState("");
+  const [todos,setTodos] = useState([])
+  
+  const addTodo = ()=>{
+     setTodos([...todos,todo])
+     setTodo('')
+
+  }
+
+  const preventSubmit=(e)=>{
+      e.preventDefault()
+
+  }
+
+  useEffect(()=>{
+   inputRef.current.focus()
+  })
+
+
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <div className="w-full max-w-md bg-white rounded-xl shadow-lg p-6">
@@ -8,14 +30,15 @@ function App() {
           TODO APP
         </h2>
 
-        <form className="flex gap-2 mb-6">
-          <input
+        <form onSubmit={preventSubmit} className="flex gap-2 mb-6">
+          <input value={todo} ref={inputRef} onChange={((e)=>{setTodo(e.target.value)})} 
             type="text"
             placeholder="Enter your message..."
             className="flex-1 border border-gray-300 rounded-lg px-4 py-2 outline-none focus:ring-2 focus:ring-blue-500"
           />
 
           <button
+            onClick={addTodo}
             className="bg-blue-600 text-white px-5 py-2 rounded-lg hover:bg-blue-700 transition"
           >
             ADD
@@ -23,26 +46,20 @@ function App() {
         </form>
 
         <ul className="space-y-3">
-          <li className="flex justify-between items-center bg-gray-100 px-4 py-3 rounded-lg shadow-sm">
-            <span>Learn React</span>
-            <button className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600 transition">
-              Delete
-            </button>
-          </li>
+         
+           {todos.map((todo,index)=>{
+              return(
 
-          <li className="flex justify-between items-center bg-gray-100 px-4 py-3 rounded-lg shadow-sm">
-            <span>Learn Tailwind CSS</span>
+              <li key={index} className="flex justify-between items-center bg-gray-100 px-4 py-3 rounded-lg shadow-sm">
+              <span>{todo}</span>
             <button className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600 transition">
               Delete
             </button>
-          </li>
-
-          <li className="flex justify-between items-center bg-gray-100 px-4 py-3 rounded-lg shadow-sm">
-            <span>Build Todo App</span>
-            <button className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600 transition">
-              Delete
-            </button>
-          </li>
+            </li>
+  
+              )
+           })}
+         
         </ul>
       </div>
     </div>
